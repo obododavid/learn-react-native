097c41101ff84c3cf7647a47a82b269f7d38738b
David Obodo <obododavid5@gmail.com>
2020-05-28 08:29:17 +0100

    add basci styling for input and button

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('My name is David oboo')
+  const [outputText, setOutputText] = useState('My name is David obodo i can give three and')
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text changed')} />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Enter course goal" style={styles.input} />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
 });
